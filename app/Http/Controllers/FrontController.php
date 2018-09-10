9aec32baba41881f536ae0ab1eb3eabed7436daf
Rodrigo <rodrigodrupal1@gmail.com>
2018-09-10 16:12:29 -0500

    Inicia proyecto solo el header

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Front;
+use App\Header;
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $header=Header::orderBy('id','DESC')->firstOrFail();
+
+       // dd($header);
+
+        return view('front.front',compact('header'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Front  $front
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Front $front)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Front  $front
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Front $front)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Front  $front
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Front $front)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Front  $front
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Front $front)
+    {
+        //
+    }
+}
